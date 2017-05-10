040eb54f025a55481dbcaf62681cd83474823376
Tim Allen <tim@noblesamurai.com>
2017-05-10 13:16:40 +1000

    Initial implementation.

---
+++ index.js
@@ -0,0 +1,17 @@
+const config = require('./config');
+const amqp = require('amqp-wrapper')(config.amqp);
+const knex = require('knex')(config.db);
+
+knex.schema.createTableIfNotExists(config.db.tableName, function (table) {
+  table.increments();
+  table.jsonb(config.db.tableName);
+  table.timestamps(true, true);
+});
+
+amqp.connect().then(function () {
+  amqp.consume(onMessage);
+}).catch(console.error);
+
+function onMessage (message, cb) {
+  knex(config.db.tableName).insert(message).asCallback(cb);
+}
