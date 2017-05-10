ca73a1578f91c91c813c0ca47244ad6a4a377723
Tim Allen <tim@noblesamurai.com>
2017-05-10 12:32:28 +1000

    Initiial commit.

---
+++ test/index.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const dirtyChai = require('dirty-chai');
+const expect = chai.expect;
+const proxyquire = require('proxyquire');
+
+chai.use(dirtyChai);
+
+describe('The service', function () {
+  it('processes a message', function () {
+    const service = pro
+  });
+});
