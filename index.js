cbf846cdc5d2c0b78b9b509145509f03299a410a
Tim Allen <tim@noblesamurai.com>
2018-08-03 15:47:48 +1000

    ability to put meta data in separate column

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const amqp = require('amqp-wrapper')(config.amqp);
 const knex = require('knex')(config.db.knex);
 const debug = require('debug')('amqp-log-to-pg');
 
+let hasMeta;
 knex.schema.createTableIfNotExists(config.db.tableName, function (table) {
   table.increments();
+  table.jsonb('meta');
   table.jsonb('data');
   table.timestamps(true, true);
 }).then(function () {
+  return knex.schema.hasColumn(config.db.tableName, 'meta');
+}).then(function (result) {
+  hasMeta = result;
   amqp.connect().then(function () {
     amqp.consume(onMessage);
   }).catch(console.error);
@@ -17,5 +22,7 @@ knex.schema.createTableIfNotExists(config.db.tableName, function (table) {
 
 function onMessage (message, cb) {
   debug('message received', message);
-  knex(config.db.tableName).insert({ data: JSON.stringify(message) }).asCallback(cb);
+  const insert = { data: JSON.stringify(message) };
+  if (hasMeta && message.meta) insert.meta = JSON.stringify(message.meta);
+  knex(config.db.tableName).insert(insert).asCallback(cb);
 }
