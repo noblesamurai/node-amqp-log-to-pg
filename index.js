35baaa677129bb6d1acf16b73c33652c5ad7e85b
Tim Allen <tim@noblesamurai.com>
2018-08-06 11:41:14 +1000

    Ability to export migrations as module.

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ const knex = require('knex')(config.db.knex);
 const debug = require('debug')('amqp-log-to-pg');
 
 let hasMeta;
-knex.schema.createTableIfNotExists(config.db.tableName, function (table) {
-  table.increments();
-  table.jsonb('meta');
-  table.jsonb('data');
-  table.timestamps(true, true);
-}).then(function () {
+async function migrate () {
+  await knex.schema.createTableIfNotExists(config.db.tableName, function (table) {
+    table.increments();
+    table.jsonb('meta');
+    table.jsonb('data');
+    table.timestamps(true, true);
+  });
   return knex.schema.hasColumn(config.db.tableName, 'meta');
-}).then(function (result) {
+}
+
+migrate().then(function (result) {
   hasMeta = result;
   amqp.connect().then(function () {
     amqp.consume(onMessage);
@@ -26,3 +29,5 @@ function onMessage (message, cb) {
   if (hasMeta && message.meta) insert.meta = JSON.stringify(message.meta);
   knex(config.db.tableName).insert(insert).asCallback(cb);
 }
+
+if (module.main !== module) module.exports.migrate = migrate;
