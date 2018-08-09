078cf34cf4e120ecae4eb1cf5d85024981ff7260
Tim Allen <tim@noblesamurai.com>
2018-08-09 10:00:55 +1000

    Use normal knex migrations.

--- index.js
+++ index.js
@@ -3,38 +3,24 @@
 const config = require('./config');
 const debug = require('debug')('amqp-log-to-pg');
 
-let hasMeta;
-function migrate (knex) {
-  return knex.schema.createTableIfNotExists(config.db.tableName, function (table) {
-    table.increments();
-    table.jsonb('meta');
-    table.jsonb('data');
-    table.timestamps(true, true);
-  }).then(() => {
-    return knex.schema.hasColumn(config.db.tableName, 'meta');
-  });
-}
-
 function main () {
   const knex = require('knex')(config.db.knex);
-  migrate(knex).then(function (result) {
+  knex.migrate.latest().then(function () {
     const amqp = require('amqp-wrapper')(config.amqp);
-    hasMeta = result;
     amqp.connect().then(function () {
       amqp.consume(onMessage);
-    }).catch(console.error);
-  });
+    });
+  }).catch(console.error);
 
   function onMessage (message, cb) {
     debug('message received', message);
     const insert = { data: JSON.stringify(message) };
-    if (hasMeta && message.meta) insert.meta = JSON.stringify(message.meta);
+    if (message.meta) insert.meta = JSON.stringify(message.meta);
     knex(config.db.tableName).insert(insert).asCallback(cb);
   }
 }
 
 if (module.main !== module) {
-  module.exports.migrate = migrate;
   module.exports.main = main;
 } else {
   main();
