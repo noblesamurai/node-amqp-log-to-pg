d0aeecfaa36a6f9b9df1d1247a5338665ae3fbf3
Tim Allen <tim@noblesamurai.com>
2020-09-08 11:10:00 +1000

    Just insert data directly, not under sub-key.

--- src/on-message.js
+++ src/on-message.js
@@ -3,7 +3,7 @@ const debug = require('debug')('amqp-log-to-amqp:on-message');
 function onMessage (knex, tableName) {
   return function (message, cb) {
     debug('message received', message);
-    const insert = { data: JSON.stringify(message) };
+    const insert = JSON.stringify(message);
     if (message.meta) insert.meta = JSON.stringify(message.meta);
     knex(tableName).insert(insert).asCallback(cb);
   };
